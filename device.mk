467d430df9f1b8bfe376f996caff6ddb34362918
Bruno Martins <bgcngm@gmail.com>
2018-04-12 11:13:39 +0100

    msm8996-common: Switch to HIDL ANT+ HAL
    Change-Id: I0e0252b6e2c7e3e7da1e8508f010b7e6c2bfd128
    Signed-off-by: Kazakov Stepan <ksrt12group@gmail.com>

--- device.mk
+++ device.mk
@@ -136,6 +136,14 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
 
+# ANT+
+PRODUCT_PACKAGES += \
+    AntHalService \
+    com.dsi.ant.antradio_library
+
+PRODUCT_COPY_FILES += \
+    external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(PERM_PATH)/com.dsi.ant.antradio_library.xml
+
 # Camera
 PRODUCT_PACKAGES += \
     camera.msm8996 \
