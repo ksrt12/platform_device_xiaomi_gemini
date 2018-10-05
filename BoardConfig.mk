f6bf71f0716b8ea32269833941cf07f3fd275fc0
Vinay Verma <vvinay@codeaurora.org>
2018-10-05 12:38:44 +0530

    msm8996-common: Remove firmware class path for kernel firmware loading
    In ueventd support has been added to read firmware path from
    vendor.It wil parse /vendor/firmware_mnt/image information
    and add in look up path/search path for loading firmware.
    Change uid of /venodr/firmware_mnt back to system as kernel
    firmware loading is removed. Previouly to support kernel firmware
    loading root permissions were required.
    
    Change-Id: I560ed75ee07626094acbc879b615190cf3a9faa3
    Signed-off-by: Kazakov Stepan <ksrt12group@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -55,7 +55,7 @@ DEVICE_MATRIX_FILE := $(VENDOR_PATH)/configs/compatibility_matrix.xml
 # Kernel
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom ehci-hcd.park=3 cma=32M@0-0xffffffff
-BOARD_KERNEL_CMDLINE += firmware_class.path=/vendor/firmware_mnt/image loop.max_part=7
+BOARD_KERNEL_CMDLINE += loop.max_part=7
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
