be1fca1ba47843e0b3de0655b5d2d06dfdec8786
Kazakov Stepan <ksrt12group@gmail.com>
2018-11-09 16:12:35 +0300

    mv sdclang props
    Signed-off-by: Kazakov Stepan <ksrt12group@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -8,7 +8,21 @@ if [ ! -e $apply ]
 then sh $apply.sh;
 touch $apply; fi;
 
+SDCLANG=true
 export SDCLANG_PATH=/home/ksrt12/u/sdclang/toolchains/llvm-Snapdragon_LLVM_for_Android_6.0/prebuilt/linux-x86_64/bin;
+export SDCLANG_LTO_DEFS=device/xiaomi/gemini/patches/sdllvm-lto-defs.mk;
 export SDCLANG_COMMON_FLAGS="-O3 -fvectorize -Wno-user-defined-warnings -Wno-vectorizer-no-neon -Wno-unknown-warning-option \
 -Wno-deprecated-register -Wno-tautological-type-limit-compare -Wno-sign-compare -Wno-gnu-folding-constant \
 -mllvm -arm-implicit-it=always -Wno-inline-asm -Wno-unused-command-line-argument -Wno-unused-variable";
+# Enable based on host OS/availablitiy
+case $(uname -s) in
+    Linux)
+        if [ -d "$SDCLANG_PATH" ]; then
+            export SDCLANG=true; echo -e "\033[32m""sdclang founded: $SDCLANG_PATH""\033[0m";
+        fi
+        ;;
+    Darwin)
+        ;;
+    *)
+        ;;
+esac
