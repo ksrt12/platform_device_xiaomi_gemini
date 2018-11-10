f6963850f86371055371c5387986c276a0b42436
Kazakov Stepan <ksrt12group@gmail.com>
2018-11-11 00:40:11 +0300

    add google bootanimation
    Signed-off-by: Kazakov Stepan <ksrt12group@gmail.com>

--- patches/apply.sh
+++ patches/apply.sh
@@ -7,7 +7,7 @@ CMBASE=$(readlink -f "$PATCHBASE/../../../../")
 for i in $(find "$PATCHBASE"/* -type d); do
 	PATCHNAME=$(basename "$i")
 	PATCHTARGET=$PATCHNAME
-	for i in $(seq 4); do
+	for i in $(seq 6); do
 		PATCHTARGET=$(echo $PATCHTARGET | sed 's/_/\//')
 		if [ -d "$CMBASE/$PATCHTARGET" ]; then break; fi
 	done
