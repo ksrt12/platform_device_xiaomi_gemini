1e01ad8eff545cef0c473ac42a3216b96c03e948
Wileen Chiu <wileenc@codeaurora.org>
2018-11-26 12:08:55 -0800

    msm8996-common: Add support for telephony hidl wrapper
    Change-Id: I4e248bb6de482d503e672d284189b24cafac8d20
    Signed-off-by: Kazakov Stepan <ksrt12group@gmail.com>

--- device.mk
+++ device.mk
@@ -362,6 +362,8 @@ PRODUCT_COPY_FILES += \
 
 # Telephony
 PRODUCT_PACKAGES += \
+    qti-telephony-hidl-wrapper \
+    qti_telephony_hidl_wrapper.xml \
     qti-telephony-utils \
     qti_telephony_utils.xml \
     telephony-ext
