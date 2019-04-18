4dee7724bfd7140a9d9b1e475709083ec25848ea
Kazakov Stepan <ksrt12group@gmail.com>
2019-04-19 00:54:53 +0300

    fix SDCLANG_LTO_DEFS path
    Signed-off-by: Kazakov Stepan <ksrt12group@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -8,9 +8,8 @@ if [ ! -e $apply ]
 then sh $apply.sh;
 touch $apply; fi;
 export changelog_days=10;
-SDCLANG=true
 export SDCLANG_PATH=/home/ksrt12/u/sdclang/toolchains/llvm-Snapdragon_LLVM_for_Android_6.0/prebuilt/linux-x86_64/bin;
-export SDCLANG_LTO_DEFS=device/xiaomi/gemini/patches/sdllvm-lto-defs.mk;
+export SDCLANG_LTO_DEFS=device/xiaomi/gemini/sdllvm-lto-defs.mk;
 export SDCLANG_COMMON_FLAGS="-O3 -fvectorize -Wno-user-defined-warnings -Wno-vectorizer-no-neon -Wno-unknown-warning-option \
 -Wno-deprecated-register -Wno-tautological-type-limit-compare -Wno-sign-compare -Wno-gnu-folding-constant \
 -mllvm -arm-implicit-it=always -Wno-inline-asm -Wno-unused-command-line-argument -Wno-unused-variable";
