39d26384a338affeeddd8c40c03fa0875fafd3dc
Kazakov Stepan <ksrt12group@gmail.com>
2019-06-15 01:19:15 +0300

    capricorn: revert back to stock sensors
    Signed-off-by: Kazakov Stepan <ksrt12group@gmail.com>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -24,7 +24,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE       := fstab.qcom
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/fstab.qcom
+LOCAL_SRC_FILES    := etc/fstab.$(LDEV).qcom
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)
 include $(BUILD_PREBUILT)
 
@@ -32,7 +32,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE       := init.qcom.rc
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.qcom.rc
+LOCAL_SRC_FILES    := etc/init.qcom.$(LDEV).rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
