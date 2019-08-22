36a1510a26c1393ad80eeb9897c0444ca83f19ee
Kazakov Stepan <ksrt12group@gmail.com>
2019-08-22 03:34:48 +0300

    build syberia capricorn
    Signed-off-by: Kazakov Stepan <ksrt12group@gmail.com>

---
+++ syberia_capricorn.mk
@@ -0,0 +1,42 @@
+#
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017-2018 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from gemini device
+$(call inherit-product, device/xiaomi/gemini/device.mk)
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := syberia_capricorn
+PRODUCT_DEVICE := capricorn
+PRODUCT_MODEL := MI 5s
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE="capricorn" \
+    PRODUCT_NAME="capricorn" \
+    PRIVATE_BUILD_DESC="capricorn-user 7.0 NRD90M V9.5.4.0.NAGMIFD release-keys"
+
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := "Xiaomi/capricorn/capricorn:7.0/NRD90M/V9.5.4.0.NAGMIFD:user/release-keys"
+
+export SKIP_ABI_CHECKS := true
+SYBERIA_BUILD_TYPE := VitaPostMortem
+
+# Inherit some common Syberia OS stuff.
+$(call inherit-product, vendor/syberia/common.mk)
