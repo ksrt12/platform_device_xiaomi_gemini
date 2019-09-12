0dcd1042aba844ca122b7cdb440c4df426cb7539
Kazakov Stepan <ksrt12group@gmail.com>
2019-09-12 22:42:42 +0300

    update vendorsetup.sh
    Signed-off-by: Kazakov Stepan <ksrt12group@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-if [ -z $BUILD_MSM4_4 ]; then 
-if [ "$LDEV" == "gemini" ]; then export BUILD_MSM4_4=true;
-else export BUILD_MSM4_4=false; fi; fi;
+if [ "$LDEV" == "gemini" ]; then BUILD_MSM4_4=true; else BUILD_MSM4_4=false; fi;
+export BUILD_MSM4_4=$BUILD_MSM4_4
 export USE_CCACHE=true;
 export SKIP_ABI_CHECKS=true;
 apply=patches/apply;
