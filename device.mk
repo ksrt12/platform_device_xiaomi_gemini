3981575ca7b4f9b0db7615dd17fbdf4f2b38d8bb
fitsuli <danilyuk_2011@bk.ru>
2019-11-19 21:08:49 +0500

    rootdir: merge and rebase gemini & capricorn configs
    Sync gemini and capricorn power setup
    Partially sync init.qcom.capricorn.rc with gemini's config
    
    Signed-off-by: Kazakov Stepan <ksrt12group@gmail.com>

--- device.mk
+++ device.mk
@@ -166,19 +166,7 @@ PRODUCT_PACKAGES += \
 
 # Device init scripts
 PRODUCT_PACKAGES += \
-    fstab.qcom \
-    init.qcom.rc \
-    init.qcom.power.rc \
-    init.qcom.usb.rc \
-    init.qcom.sh \
-    ueventd.qcom.rc
-
-ifeq ($(LDEV), capricorn)
-PRODUCT_PACKAGES += \
-    init.target.rc
-endif
-#    init.spectrum.rc \
-#    init.spectrum.sh
+    rootdir
 
 # CNE
 PRODUCT_PACKAGES += \
